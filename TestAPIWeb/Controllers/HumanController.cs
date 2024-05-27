eccc520f008687dbe45557391387e0302b077ee9
shanghaik168@gmail.com <Shanghaik1998>
2024-05-27 08:37:01 +0700

    code API practice

---
+++ TestAPIWeb/Controllers/HumanController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+namespace TestAPIWeb.Controllers
+{
+    public class HumanController : Controller
+    {
+        public IActionResult Index()
+        {
+            string urlRequest = @"https://localhost:7002/api/Practice/get-list-human";
+            var response = new HttpClient().GetStringAsync(urlRequest).Result;
+            List<Human> result = JsonConvert.DeserializeObject<List<Human>>(response);
+            return View(result);
+        }
+    }
+    public class Human
+    {
+        public string Name { get; set; }
+        public string CCCD { get; set; }
+        public int Age { get; set; }
+    }
+}
